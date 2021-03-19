eb80e8c605e9e0748f11a0cbc676afc9c4c02b75
Raikovm <36475061+Raikovm@users.noreply.github.com>
2021-03-19 18:20:54 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IS_Lab1
+{
+    public partial class Form1 : Form
+    {
+
+       const byte IntQstCount = 2;
+       const byte QltQstCount = 4;
+       float[] Questions = new float[IntQstCount + QltQstCount];
+       const int MaxScore = 16;
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public double Result()
+        {
+
+            double InterfaceResult = 0;
+            double QualityResult = 0;
+            for (int i = 0; i < IntQstCount; i++)
+                InterfaceResult += Questions[i];
+
+            for (int i = IntQstCount; i < Questions.Length; i++)
+                QualityResult += Questions[i];
+
+
+            return InterfaceResult + QualityResult;
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void bt_Done_Click(object sender, EventArgs e)
+        {
+            if (Result() >= MaxScore * 0.75)
+                MessageBox.Show("Требования заказчика удовлетворены", "Результат");
+            else if (Result() >= MaxScore * 0.50)
+                MessageBox.Show("Программе требуется частичная доработка", "Результат");
+            else
+                MessageBox.Show("Программа нуждается в полной переработке", "Результат");
+
+        }
+
+        private void cb_Q1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string caseSwitch = cb_Q1.Text;
+            switch (caseSwitch)
+            {
+                case "Отлично":
+                    Questions[0] = 3;                        
+                    break;
+                case "Хорошо":
+                    Questions[0] = 2; 
+                    break;
+                case "Удовлетворительно":
+                    Questions[0] = 1;
+                    break;
+                case "Плохо":
+                    Questions[0] = 0;
+                    break;
+                default:
+                    Questions[0] = 0;
+                    break;
+            }
+//            lb_Done.Text = Questions[0].ToString();
+        }
+
+        private void cb_Q2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string caseSwitch = cb_Q2.Text;
+            switch (caseSwitch)
+            {
+                case "Отлично":
+                    Questions[1] = 3;
+                    break;
+                case "Хорошо":
+                    Questions[1] = 2;
+                    break;
+                case "Удовлетворительно":
+                    Questions[1] = 1;
+                    break;
+                case "Плохо":
+                    Questions[1] = 0;
+                    break;
+                default:
+                    Questions[1] = 0;
+                    break;
+            }
+        }
+
+        private void cb_Qlt_Q1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string caseSwitch = cb_Qlt_Q1.Text;
+            switch (caseSwitch)
+            {
+                case "Отлично":
+                    Questions[2] = 3;
+                    break;
+                case "Хорошо":
+                    Questions[2] = 2;
+                    break;
+                case "Удовлетворительно":
+                    Questions[2] = 1;
+                    break;
+                case "Плохо":
+                    Questions[2] = 0;
+                    break;
+                default:
+                    Questions[2] = 0;
+                    break;
+            }
+        }
+
+        private void cb_Qlt_Q2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string caseSwitch = cb_Qlt_Q2.Text;
+            switch (caseSwitch)
+            {
+                case "Совместима со многими старыми версиями ОС":
+                    Questions[3] = 2;
+                    break;
+                case "Совместима со свежими версиями ОС":
+                    Questions[3] = 1;
+                    break;
+                case "Совместима только с текущей версией ОС":
+                    Questions[3] = 0;
+                    break;
+                default:
+                    Questions[3] = 0;
+                    break;
+            }
+        }
+
+        private void cb_Qlt_Q3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string caseSwitch = cb_Qlt_Q3.Text;
+            switch (caseSwitch)
+            {
+                case "Отсутствуют":
+                    Questions[4] = 3;
+                    break;
+                case "Незначительно":
+                    Questions[4] = 2;
+                    break;
+                case "Много":
+                    Questions[4] = 1;
+                    break;
+                case "Критичное":
+                    Questions[4] = 0;
+                    break;
+                default:
+                    Questions[4] = 0;
+                    break;
+            }
+        }
+
+        private void cb_Qlt_Q4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string caseSwitch = cb_Qlt_Q4.Text;
+            switch (caseSwitch)
+            {
+                case "Сильная":
+                    Questions[5] = 2;
+                    break;
+                case "Средняя ":
+                    Questions[5] = 1;
+                    break;
+                case "Слабая":
+                    Questions[5] = 0;
+                    break;
+                default:
+                    Questions[5] = 0;
+                    break;
+            }
+        }
+    }
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("IS_Lab1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IS_Lab1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("9d857a7a-e4f3-4339-9e3f-be59ac879ec3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
